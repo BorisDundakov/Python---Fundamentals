d53172d0b8098e54236f5448be04924802154f96
b0reto <71731579+b0reto@users.noreply.github.com>
2021-06-04 12:54:33 +0300

    Add files via upload

---
+++ 08 - Text Processing - Lab/String_to_Num_converter.py
@@ -0,0 +1,19 @@
+# print("Enter the string below:")
+# a = input()
+#
+#
+# if a.isdigit():
+#     print(float(a))
+#     print("Successfull operation")
+# else:
+#     print("String cannot be converted to number")
+
+
+
+text = "Ivan Ivanov"
+
+text_replaced = text.replace("Ivanov", "Georgiev")
+
+print(text)
+print(text_replaced)
+print(text)
